80b0a55d90acb681abddc6e1bf4af5771bb8be93
jtomas <jtomas@upc.es>
2022-12-17 03:20:01 +0100

    8.-Geolocalización

--- app/src/main/java/com/example/examentoms/Tab1.java
+++ app/src/main/java/com/example/examentoms/Tab1.java
@@ -1,17 +1,29 @@
 package com.example.examentoms;
 
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class Tab1 extends Fragment  {
+public class Tab1 extends Fragment implements LocationListener {
 
+    // 8.- Geolocalización CUIDADO: Para que funcione hay que dar permiso manualmente en el dispositivo
+    LocationManager manejador;
+    TextView latitud, longitud;
     Button button1;
     EditText editText;
 
@@ -27,6 +39,9 @@ public class Tab1 extends Fragment  {
         // 3.- Propiedades vistas
         button1 = view.findViewById(R.id.button1);
         editText = view.findViewById(R.id.editText);
+        latitud = view.findViewById(R.id.latitud);
+        longitud = view.findViewById(R.id.longitud);
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +53,28 @@ public class Tab1 extends Fragment  {
                 }
             }
         });
+        // 8.- Geolocalización
+        manejador = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
         return view;
     }
+
+    // 8.- Geolocalización
+    @SuppressLint("MissingPermission")
+    @Override public void onResume() {
+        super.onResume();
+        manejador.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1,this);
+    }
+
+    @Override public void onPause() {
+        super.onPause();
+        manejador.removeUpdates(this);
+        Log.e("Examen", "Se dejan de recibir actualizaciones de posición");
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitud.setText("" + location.getLatitude());
+        longitud.setText("" + location.getLongitude());
+    }
+
 }
